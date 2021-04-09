bb822b1ff84709553b89f691e6f8f5f4b663c647
ahmad.NetDeveloper@Gmail.com <ahmad.NetDeveloper@Gmail.com>
2021-04-09 21:56:20 +0300

    ai-feat-issue-generalSettings-15

--- src/app/features/app/app.module.ts
+++ src/app/features/app/app.module.ts
@@ -20,6 +20,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { MatPaginatorIntlService } from '@app/infrastructure/shared/Services/mat-paginator-intl.service';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
     declarations: [AppComponent, mainComponents],
@@ -54,6 +55,10 @@ import { MatPaginatorIntlService } from '@app/infrastructure/shared/Services/mat
         {
             provide: MatPaginatorIntl,
             useClass: MatPaginatorIntlService,
+        },
+        {
+            provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+            useValue: { appearance: 'outline' },
         },
     ],
     bootstrap: [AppComponent],
--- src/app/infrastructure/models/SystemEnum.ts
+++ src/app/infrastructure/models/SystemEnum.ts
@@ -1,3 +1,10 @@
 export enum NotificationType {
     Chatting = 10,
 }
+
+export enum DefaultStatusNumber {
+    Free = 1,
+    Official = 2,
+    Personal = 3,
+    Unknown = 4,
+}
